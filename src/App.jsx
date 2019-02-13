ea9ccd2ed6d5a7af59fe417e511a91b8e0724c23
Terence Yau <tyauzer@hotmail.com>
2019-02-13 03:59:49 +0000

    added new message functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,75 @@ import MessageList from './MessageList.jsx';
 
 export default class App extends Component {
   constructor(props) {
-  super(props);
-   this.state =
+    super(props);
+    this.state =
       {
         currentUser: { name: 'Bob' },
         messages: [
-          { id: '1',
+          {
+            id: '1',
             username: 'Bob',
             content: 'Has anyone seen my marbles?',
           },
-          { id: '2',
+          {
+            id: '2',
             username: 'Anonymous',
             content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
           }
-        
-        ]
+        ],
+        nextMsg: ''
       };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleChange(event) {
+    // const newMsg = this.state.messages
+    const nextMsg = event.target.value;
+    this.setState({ nextMsg });
+  }
+
+  handleKeyPress(event) {
+    const keyPressed = event.key;
+    if (keyPressed === 'Enter') {
+      this.setState({
+        messages: this.state.messages.concat({
+          id: Math.random(),
+          username: this.state.currentUser.name,
+          content: this.state.nextMsg,
+        })
+      })
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount <App />');
+    setTimeout(() => {
+      console.log('Simulating incoming message');
+      // Add a new message to the list of messages in the data store
+      const newMessage = { id: 3, username: 'Michelle', content: 'Hello there!' };
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messages: messages })
+    }, 3000);
   }
 
   render() {
+    console.log(this.state.nextMsg);
     return (
       <div>
         <div>
           <MessageList messages={this.state.messages} />
         </div>
         <div>
-          <ChatBar username={this.state.currentUser.name} />
+          <ChatBar
+            username={this.state.currentUser.name}
+            handleChange={this.handleChange}
+            handleKeyPress={this.handleKeyPress}
+          />
         </div>
-        
+
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -6,11 +6,17 @@ export default class ChatBar extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.props.username}/>
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    </footer>
+        <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.props.username} />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          onChange={this.props.handleChange} 
+          onKeyPress={this.props.handleKeyPress}
+          />
+      </footer>
     )
   }
 
